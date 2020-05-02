30ec7c9bf5d47a48a80a9999f354b571e8228d56
Harvey Liu <harveyliu@kkstream.com.tw>
2020-05-03 03:05:33 +0800

    tuning TimeList component

--- src/components/TimeList/Readme.md
+++ src/components/TimeList/Readme.md
@@ -1,18 +1,34 @@
 Basic:
 
 ```jsx
-<TimeList
-  availableTimes={Array.from(Array(10).keys()).map(i => i * 3600000)}
-  bookedTimes={Array.from(Array(5).keys()).map(i => i * 2 * 3600000)}
+import moment from 'moment'
+
+;<TimeList
+  available={Array.from(Array(5).keys()).map(i => ({
+    start: moment().startOf('day').add(i * 4, 'hour').toISOString(),
+    end: moment().startOf('day').add(i * 4 + 1, 'hour').toISOString()
+  }))}
+  booked={Array.from(Array(5).keys()).map(i => ({
+    start: moment().startOf('day').add(i * 4 + 2, 'hour').toISOString(),
+    end: moment().startOf('day').add(i * 4 + 3, 'hour').toISOString()
+  }))}
 />
 ```
 
 It is formatted through moment.js, so you can change the format according to [moment.js document](https://momentjs.com/docs/#/displaying/format/).
 
 ```jsx
-<TimeList
+import moment from 'moment'
+
+;<TimeList
   format="HH:mm:ss"
-  availableTimes={Array.from(Array(10).keys()).map(i => i * 1805000)}
-  bookedTimes={Array.from(Array(5).keys()).map(i => i * 2 * 1805000)}
+  available={Array.from(Array(5).keys()).map(i => ({
+    start: moment().startOf('day').add(i * 4, 'hour').toISOString(),
+    end: moment().startOf('day').add(i * 4 + 1, 'hour').toISOString()
+  }))}
+  booked={Array.from(Array(5).keys()).map(i => ({
+    start: moment().startOf('day').add(i * 4 + 2, 'hour').toISOString(),
+    end: moment().startOf('day').add(i * 4 + 3, 'hour').toISOString()
+  }))}
 />
 ```
--- src/components/TimeList/TimeList.jsx
+++ src/components/TimeList/TimeList.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
-import classnames from 'classnames'
 
 const styles = {
   textAlign: 'center',
   listStyleType: 'none',
+  padding: 0,
+  margin: 0,
   item: {
     border: '1px solid transparent',
     boxSizing: 'border-box',
     padding: '3px 0',
     color: '#02cab9',
     fontWeight: 700,
+    fontSize: '.75rem',
     disabled: {
       color: '#b6b6b6',
       fontWeight: 400
@@ -19,42 +21,59 @@ const styles = {
   }
 }
 
-const TimeList = ({ format, availableTimes, bookedTimes }) => {
-  const bookedTimeset = new Set(bookedTimes)
+const TimeList = ({ format, date, available, booked }) => {
+  const momentDate = moment(date).startOf('day')
+  const list = Array.from(Array(48).keys())
+    .map((i) => momentDate.clone().add(i * 30, 'minute'))
+    .map((time) => ({
+      time: time,
+      isAvailable: available.some((item) =>
+        time.isBetween(item.start, item.end, null, '[)')
+      ),
+      isBooked: booked.some((item) =>
+        time.isBetween(item.start, item.end, null, '[)')
+      )
+    }))
+    .filter((item) => item.isAvailable || item.isBooked)
   return (
     <ul className='time-list' style={styles}>
-      {availableTimes.map((time) => {
-        const isReserved = bookedTimeset.has(time)
-        return (
-          <li
-            key={time}
-            className={classnames(
-              'time-list--item',
-              isReserved && 'time-list--item-disabled'
-            )}
-            style={{
-              ...styles.item,
-              ...(isReserved && styles.item.disabled)
-            }}
-          >
-            {moment(time).format(format)}
-          </li>
-        )
-      })}
+      {list.map((item) => (
+        <li
+          key={item.time.toISOString()}
+          style={{
+            ...styles.item,
+            ...(item.isBooked && styles.item.disabled)
+          }}
+        >
+          {moment(item.time).format(format)}
+        </li>
+      ))}
     </ul>
   )
 }
 
 TimeList.propTypes = {
   format: PropTypes.string,
-  availableTimes: PropTypes.arrayOf(PropTypes.number),
-  bookedTimes: PropTypes.arrayOf(PropTypes.number)
+  date: PropTypes.instanceOf(Date),
+  available: PropTypes.arrayOf(
+    PropTypes.shape({
+      start: PropTypes.string,
+      end: PropTypes.string
+    })
+  ),
+  booked: PropTypes.arrayOf(
+    PropTypes.shape({
+      start: PropTypes.string,
+      end: PropTypes.string
+    })
+  )
 }
 
 TimeList.defaultProps = {
   format: 'HH:mm',
-  availableTimes: [],
-  bookedTimes: []
+  date: new Date(),
+  available: [],
+  booked: []
 }
 
 export default TimeList
